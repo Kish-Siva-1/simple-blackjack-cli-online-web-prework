8936745000bec5b5ce30dad07c8a0eb6f49ae54e
dependable-inventor-4583 <rodes88@msn.com>
2019-04-07 01:10:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,62 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
-end
-
-def display_card_total
-  # code #display_card_total here
+  rand(1..11)
 end
 
 def prompt_user
-  # code #prompt_user here
+  
+  puts "Type 'h' to hit or 's' to stay"
+
+end
+
+def display_card_total(card_total)
+    puts "Your cards add up to #{card_total}"
 end
 
 def get_user_input
-  # code #get_user_input here
+    gets.chomp 
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  
+    num1 = deal_card
+    num2 = deal_card
+    total = num1 + num2
+    
+    display_card_total(total)
+    
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  
+prompt_user
+get_user_input
+
+  if get_user_input == 'h'
+      num1 = deal_card
+      total = total + num1
+      
+  elsif get_user_input == 's'
+  
+  else 
+      invalid_command
+      prompt_user
+      
+  end
+  
+  display_card_total(total)
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
